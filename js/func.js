0a28c3463c0ca19f3e7ce0811240ac582835e7a6
mehdiMj <mehdi.is@live.com>
2023-09-30 22:55:42 +0330

    fix: :truck: moved to sub directories

--- js/func.js
+++ js/func.js
@@ -15,17 +15,3 @@ document.getElementById('genString').textContent = genString();
 // Get local time
 let IR_TIME = new Date().toLocaleString("fa-IR", { timeZone: "Asia/Tehran" });
 document.getElementById('irTime').textContent = IR_TIME + ' Teh';
-
-// cloudflare real IP
-(function () {
-    function d() {
-        var b = a.getElementById("cf-footer-item-ip"),
-            c = a.getElementById("cf-footer-ip-reveal");
-        b && "classList" in b && (b.classList.remove("hidden"), c.addEventListener("click", function () {
-            c.classList.add("hidden");
-            a.getElementById("cf-footer-ip").classList.remove("hidden")
-        }))
-    }
-    var a = document;
-    document.addEventListener && a.addEventListener("DOMContentLoaded", d)
-})();
